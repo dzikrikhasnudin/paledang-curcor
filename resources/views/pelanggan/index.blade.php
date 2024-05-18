bf86d7b6fd8c88fccf27c91acf6b98f711c6177e
Dzikri Khasnudin <dzikri.khasnudin2@gmail.com>
2024-05-18 21:04:21 +0700

    konfigurasi laravel vite dan laragon

--- resources/views/pelanggan/index.blade.php
+++ resources/views/pelanggan/index.blade.php
@@ -121,7 +121,7 @@
                 </div>
                 <nav class=" space-y-3 md:space-y-0 p-4" aria-label="Table navigation">
 
-                    {{ $clients->links() }}
+                    {{ $clients->links(data: ['scrollTo' => false]) }}
                 </nav>
             </div>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+
+import fs from 'fs';
+
+const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        port: env.VITE_ASSET_PORT,
+        strictPort: true,
+        hmr: {
+            host: env.VITE_ASSET_HOST,
+            port: env.VITE_ASSET_PORT,
+        }
+    },
 });
